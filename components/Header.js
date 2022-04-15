a0131f27071b40fe1a9940b00e8a3efac2e73a7f
renaultluk <ren66.rl@gmail.com>
2022-04-15 14:18:51 +0800

    install libraries and made home page

---
+++ components/Header.js
@@ -0,0 +1,24 @@
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
+
+const Header = () => {
+    return (
+        <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="/">RL</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="/about">About Me</Nav.Link>
+                    <NavDropdown title="My Work" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.1">Web Development</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">Product Design & Embedded Systems</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3">Graphic Design & Digital Marketing</NavDropdown.Item>
+                    </NavDropdown>
+                    <Nav.Link href="/contact">Contact Me</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    )
+}
+
+export default Header;
