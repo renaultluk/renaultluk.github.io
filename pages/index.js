d89eab0b2be7374aa2f6dffb138394b34949585a
renaultluk <ren66.rl@gmail.com>
2022-04-16 17:28:31 +0800

    edit banner

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
+import { Button } from 'react-bootstrap'
 import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 import Typed from 'react-typed'
 
@@ -8,12 +9,14 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   const [offsetY, setOffsetY] = useState(0);
+  // let screenHeight = 0;
 
   const handleScroll = () => {
     setOffsetY(window.pageYOffset);
   }
 
   useEffect(() => {
+    // screenHeight = window.innerHeight;
     window.addEventListener('scroll', handleScroll);
     return () => {
       window.removeEventListener('scroll', handleScroll);
@@ -52,64 +55,27 @@ export default function Home() {
           </p>
         </h1>
 
-        <div className={styles.grid} style={{transform: `translateY(-${offsetY * 1.2}px)`  }}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercelu/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className={styles.banner} style={{ 
+          transform: `translateY(-${offsetY * 1.2}px)`,
+          height: `${500}px`,
+          // backgroundColor: `rgba(255, 255, 255, ${offsetY / screenHeight})`,
+        }}>
+          <h2>About Me</h2>
+          <p>Check out more about my experience and achievements.</p>
+          <Button variant="primary" href="/about" style={{ width: "fit-content" }}>
+            More About Me &rarr;
+          </Button>
         </div>
 
-        <div className={styles.grid} style={{transform: `translateY(-${offsetY * 0.5}px)`  }}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercelu/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className={styles.banner} style={{
+          transform: `translateY(-${offsetY * 0.5}px)`,
+          height: `${500}px`,
+        }}>
+          <h2>Projects</h2>
+          <p>Check out some of my projects.</p>
+          <Button variant="primary" href="/work" style={{ width: "fit-content" }}>
+            Projects &rarr;
+          </Button>
         </div>
         
         {/* <Parallax pages={2}>
