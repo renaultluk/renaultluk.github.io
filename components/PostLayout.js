6591c781ab924ae72c8944396c67a585b67a9639
renaultluk <71740121+renaultluk@users.noreply.github.com>
2022-06-21 22:32:31 +0800

    Edit post layout

--- components/PostLayout.js
+++ components/PostLayout.js
@@ -1,22 +1,29 @@
+import Head from 'next/head';
 import Image from 'next/image';
 
 import styles from '../styles/PostStyles.module.css';
 
 const PostLayout = ({ children, title, slug, description, thumbnail, finishDate }) => {
     return (
-        <main className={styles.postMain}>
-            <h1 className={styles.postTitle}>{title}</h1>
-            <h2 className={styles.postDes}>{description}</h2>
-            <p className={styles.finishDate}>{finishDate}</p>
-            <img 
-                width="100%"
-                height={300}
-                src={`/projects/${slug}/${thumbnail}`} 
-                alt={thumbnail} 
-                className={styles.postThumbnail} 
-            />
-            {children}
-        </main>
+        <>
+            <Head>
+                <title>{title} | Renault Luk</title>
+            </Head>
+            
+            <main className={styles.postMain}>
+                <h1 className={styles.postTitle}>{title}</h1>
+                <h2 className={styles.postDes}>{description}</h2>
+                <p className={styles.finishDate}>{finishDate}</p>
+                <img 
+                    width="100%"
+                    height={300}
+                    src={`/projects/${slug}/${thumbnail}`} 
+                    alt={thumbnail} 
+                    className={styles.postThumbnail} 
+                />
+                {children}
+            </main>
+        </>
     );
 }
 
--- pages/work/index.js
+++ pages/work/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import { Container, Card, Row, Col } from "react-bootstrap";
 // import { getPostsMetaData } from "../../utils/getPostsData";
 import getPosts from "../../scripts/fileSystem";
@@ -8,30 +9,36 @@ const WorkHome = ({ posts }) => {
     const router = useRouter();
     
     return (
-        <Container>
-            <h1>My Projects</h1>
-            <Container fluid>
-                <Row className="justify-content-md-center">
-                    {posts.map((post, index) => {
-                        return (
-                            <Col xs={12} md={6} lg={3}>
-                                <Card
-                                    key = {index}
-                                    style={{ maxWidth: '18rem', margin: '10px' }}
-                                    onClick={() => router.push(`/work/${post.data.slug}`)}
-                                >
-                                    <Card.Img style={{ height: '10rem', objectFit: 'cover' }} variant="top" src={`projects/${post.data.slug}/${post.data.thumbnail}`} />
-                                    <Card.Body>
-                                        <Card.Title>{post.data.title}</Card.Title>
-                                        <Card.Text>{post.data.description}</Card.Text>
-                                    </Card.Body>
-                                </Card>
-                            </Col>
-                        )
-                    })}
-                </Row>
+        <>
+            <Head>
+                <title>My Work | Renault Luk</title>
+            </Head>
+
+            <Container>
+                <h1>My Projects</h1>
+                <Container fluid>
+                    <Row className="justify-content-md-center">
+                        {posts.map((post, index) => {
+                            return (
+                                <Col xs={12} md={6} lg={3}>
+                                    <Card
+                                        key = {index}
+                                        style={{ maxWidth: '18rem', margin: '10px' }}
+                                        onClick={() => router.push(`/work/${post.data.slug}`)}
+                                    >
+                                        <Card.Img style={{ height: '10rem', objectFit: 'cover' }} variant="top" src={`projects/${post.data.slug}/${post.data.thumbnail}`} />
+                                        <Card.Body>
+                                            <Card.Title>{post.data.title}</Card.Title>
+                                            <Card.Text>{post.data.description}</Card.Text>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                            )
+                        })}
+                    </Row>
+                </Container>
             </Container>
-        </Container>
+        </>
     )
 }
 
