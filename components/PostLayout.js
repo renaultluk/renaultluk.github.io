0de869a225e0f595ea087ac18d007423262dd73b
renaultluk <ren66.rl@gmail.com>
2022-06-23 13:57:27 +0800

    Update post content

--- components/PostLayout.js
+++ components/PostLayout.js
@@ -1,5 +1,6 @@
-import Head from 'next/head';
+    import Head from 'next/head';
 import Image from 'next/image';
+import { Container } from 'react-bootstrap';
 
 import styles from '../styles/PostStyles.module.css';
 
@@ -10,19 +11,21 @@ const PostLayout = ({ children, title, slug, description, thumbnail, finishDate 
                 <title>{title} | Renault Luk</title>
             </Head>
             
-            <main className={styles.postMain}>
-                <h1 className={styles.postTitle}>{title}</h1>
-                <h2 className={styles.postDes}>{description}</h2>
-                <p className={styles.finishDate}>{finishDate}</p>
-                <img 
-                    width="100%"
-                    height={300}
-                    src={`/projects/${slug}/${thumbnail}`} 
-                    alt={thumbnail} 
-                    className={styles.postThumbnail} 
-                />
-                {children}
-            </main>
+            <Container sm={5}>
+                <main className={styles.postMain}>
+                    <h1 className={styles.postTitle}>{title}</h1>
+                    <h2 className={styles.postDes}>{description}</h2>
+                    <p className={styles.finishDate}>{finishDate}</p>
+                    <img 
+                        width="100%"
+                        height={300}
+                        src={`/projects/${slug}/${thumbnail}`} 
+                        alt={thumbnail} 
+                        className={styles.postThumbnail} 
+                    />
+                    {children}
+                </main>
+            </Container>
         </>
     );
 }
