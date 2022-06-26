d53f8a2e6097539ca0a73e9d712f465a8d8723b4
renaultluk <ren66.rl@gmail.com>
2022-06-26 17:58:19 +0800

    Create contact.js

---
+++ pages/contact.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import { Container } from 'react-bootstrap';
+
+const Contact = () => {
+    return (
+        <>
+            <Head>
+                <title>Contact Me | Renault Luk</title>
+            </Head>
+
+            <Container>
+                <h1>Contact Me</h1>
+                <span>Work</span>
+                
+                <span>Socials</span>
+            </Container>
+        </>
+    );
+}
+
+export default Contact;
