0280a762dd43f1bc77f153e1170d8c576502d933
renaultluk <71740121+renaultluk@users.noreply.github.com>
2022-09-03 16:54:33 +0800

    Update link styling

--- components/MDXComponentMapping.js
+++ components/MDXComponentMapping.js
@@ -1,5 +1,12 @@
+const Anchor = (props) => {
+    return (
+        <a target="_blank" rel="noopener noreferrer" style={{ color: "#D295BF" }} {...props} />
+    );
+}
+
 const MDXComponentMapping = {
     h1: ({children}) => <h1>{children}</h1>,
+    a: Anchor,
 };
 
 export default MDXComponentMapping;
